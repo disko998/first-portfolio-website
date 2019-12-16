f82c53368379045dd30889a11cff3051ee819858
Stefan <disko998@gmail.com>
2019-12-16 13:06:49 +0100

    new header style

--- js/interface.js
+++ js/interface.js
@@ -1,26 +1,25 @@
-    // toggle class
-    $(document).ready(function(){
-        $('.menu-icon').on('click', function(){
-            $(".nav-links").slideToggle('medium', function() {
-                if ($(this).is(':visible'))
-                    $(this).css('display','flex')
-            });
-        })
-
-        window.addEventListener('resize', function(size){
-            if (window.innerWidth > 847) {
-                $(".nav-links").css('display', 'flex')
-            } else {
-                $(".nav-links").css('display', 'none')
-            }
+// toggle class
+$(document).ready(function() {
+    $('.menu-button').on('click', function() {
+        $('.links').slideToggle('medium', function() {
+            if ($(this).is(':visible')) $(this).css('display', 'flex')
         })
     })
 
-    // on scroll
-    $(window).on('scroll', function() {
-        if ($(window).scrollTop()) {
-            $('nav').addClass('scroll')
+    window.addEventListener('resize', function(size) {
+        if (window.innerWidth > 847) {
+            $('.links').css('display', 'flex')
         } else {
-            $('nav').removeClass('scroll')
+            $('.links').css('display', 'none')
         }
     })
+})
+
+// on scroll
+$(window).on('scroll', function() {
+    if ($(window).scrollTop()) {
+        $('nav').addClass('scroll')
+    } else {
+        $('nav').removeClass('scroll')
+    }
+})

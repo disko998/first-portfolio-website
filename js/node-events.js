2e070525336b4d2cda38ec317f7f36a92796ef98
Stefan <disko998@gmail.com>
2020-01-13 21:13:29 +0100

    new skills section

--- js/node-events.js
+++ js/node-events.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+    displaySkills()
+
     $('.menu-button').on('click', function() {
         $('.links').slideToggle('medium', function() {
             if ($(this).is(':visible')) $(this).css('display', 'flex')
@@ -23,3 +25,13 @@ $(window).on('scroll', function() {
         $('nav').removeClass('scroll')
     }
 })
+
+function displaySkills() {
+    $.getJSON('../skills.json', function(data) {
+        $.each(data, function(key, skill) {
+            $('#skills-wrapper').append(
+                `<div class='${skill.category} skill-card'><img src="${skill.logo}" alt="test" /><h1>${skill.name}</h1></div>`,
+            )
+        })
+    })
+}

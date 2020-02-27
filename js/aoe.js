6bfeaba214e706019689c1ca018c9ac5f936fcf3
disko998 <disko998@gmail.com>
2020-02-27 03:34:59 +0100

    add animation on section title

--- js/aoe.js
+++ js/aoe.js
@@ -11,7 +11,7 @@ AOS.init({
 
     // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
     offset: 120, // offset (in px) from the original trigger point
-    delay: 100, // values from 0 to 3000, with step 50ms
+    delay: 300, // values from 0 to 3000, with step 50ms
     duration: 1000, // values from 0 to 3000, with step 50ms
     easing: 'ease', // default easing for AOS animations
     once: true, // whether animation should happen only once - while scrolling down
